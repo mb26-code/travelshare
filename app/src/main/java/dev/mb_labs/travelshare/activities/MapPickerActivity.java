3cc8a7b26b948006ded0be1b97c18a2451efa11f
Mehdi Bakhtar <mehdibakhtar26@gmail.com>
2026-01-11 22:28:04 +0100

    fixed map pick location screen where the confirm button was behind the system bottom bar

--- app/src/main/java/dev/mb_labs/travelshare/activities/MapPickerActivity.java
+++ app/src/main/java/dev/mb_labs/travelshare/activities/MapPickerActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,7 +30,14 @@ public class MapPickerActivity extends AppCompatActivity implements OnMapReadyCa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_map_picker);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_map_picker), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         double lat = getIntent().getDoubleExtra("LATITUDE", 0);
         double lng = getIntent().getDoubleExtra("LONGITUDE", 0);
