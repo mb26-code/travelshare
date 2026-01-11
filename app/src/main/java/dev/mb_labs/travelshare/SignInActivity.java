e2d137971cd63ed48eb452f23d47aa522b69a5ff
Mehdi Bakhtar <mehdibakhtar26@gmail.com>
2026-01-11 04:18:55 +0100

    added edge to edge and set padding relative to system bars to sign-in screen (forgot it previously) + an id to the "forgot password" text view

--- app/src/main/java/dev/mb_labs/travelshare/SignInActivity.java
+++ app/src/main/java/dev/mb_labs/travelshare/SignInActivity.java
@@ -8,7 +8,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.security.crypto.EncryptedSharedPreferences;
 import androidx.security.crypto.MasterKey;
 
@@ -32,8 +36,15 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_sign_in), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
         eMailField = findViewById(R.id.eMailField);
         passwordField = findViewById(R.id.passwordField);
         signInButton = findViewById(R.id.signInButton);
